dbb8342586d854b6c12399bec32e3e8577b4f538
Leo21897041 <leosaporito16@gmail.com>
2026-06-02 12:12:40 -0400

    Added sprites and bounds using Mathf.Clamp

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float moveSpeed;
+    [SerializeField] float leftBoundPadding;
+    [SerializeField] float rightBoundPadding;
+
+    [SerializeField] float topBoundPadding;
+    [SerializeField] float bottomBoundPadding;
+
+    InputAction moveAction;
+
+    Vector3 moveVector;
+
+    Vector2 minBounds;
+    Vector2 maxBounds;
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+
+        InitialBounds();
+    }
+    
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void InitialBounds()
+    { 
+        Camera mainCamera = Camera.main;
+
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+
+    void MovePlayer()
+    {
+        moveVector = moveAction.ReadValue<Vector2>();
+
+        Vector3 newPos = transform.position + moveVector * moveSpeed * Time.deltaTime;
+
+        newPos.x = Mathf.Clamp(newPos.x, minBounds.x + leftBoundPadding, maxBounds.x - rightBoundPadding);
+        newPos.y = Mathf.Clamp(newPos.y, minBounds.y + bottomBoundPadding, maxBounds.y - topBoundPadding);
+
+        transform.position = newPos;
+    }
+}
